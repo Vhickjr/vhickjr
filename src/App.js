5f97198a633e6bdac0c86c1ac425ecfc5716a30f
victor mathias <victormathias001@gmail.com>
2022-08-08 14:07:57 +0100

    coming soon2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from "./logo.svg";
+import pizza1 from "./pizza1.png";
 import "./App.css";
+import Mailer from "./components/mailer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <p className="vic2">Work in progress,</p>{" "}
-          <p className="vic1"> check back soon.</p>
-        </p>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={pizza1} className="App-logo" alt="logo" />
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <p>
+            <p className="vic2">something's cooking,</p>{" "}
+            <p className="vic1"> check back soon :)</p>
+          </p>
+        </header>
+        <div className="containerr">
+          <Mailer></Mailer>
+        </div>
+      </div>
     </div>
   );
 }
