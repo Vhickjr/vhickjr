1186bb925e0b8316305d42ad751a71095b71aa8b
victor mathias <victormathias001@gmail.com>
2022-09-03 13:24:09 +0100

    updates

--- src/pages/Home/Section/Section3/Section3.js
+++ src/pages/Home/Section/Section3/Section3.js
@@ -1,23 +1,73 @@
-import ProgressBar from "react-bootstrap/ProgressBar";
 import "./section3.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function Section3() {
-  const now = 95;
-  const now2 = 85;
-  const now3 = 70;
-  const now4 = 50;
   return (
     <div className="sec3">
-      <h3>HTML</h3>
-      <ProgressBar variant="success" now={95} label={`${now}%`} />
+      <h3 style={{ color: "white", textAlign: "center" }}>skills</h3>
+      <h3 style={{ color: "white", textAlign: "start" }}>HTML</h3>
+      <div class="progress">
+        <div
+          class="progress-bar bg-success"
+          role="progressbar"
+          style={{ width: "25%" }}
+          aria-valuenow="25"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          {" "}
+          25%
+        </div>
+      </div>
       <br></br>
-      <h3>CSS</h3>
-      <ProgressBar variant="info" now2={now2} label={`${now2}%`} />
+
+      <h3 style={{ color: "white", textAlign: "start" }}>CSS</h3>
+      <div class="progress">
+        <div
+          class="progress-bar bg-success"
+          role="progressbar"
+          style={{ width: "50%" }}
+          aria-valuenow="50"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          {" "}
+          50%
+        </div>
+      </div>
       <br></br>
-      <h3>Javascript</h3>
-      <ProgressBar variant="warning" now3={now3} label={`${now3}%`} />
+
+      <h3 style={{ color: "white", textAlign: "start" }}>Javascript</h3>
+      <div class="progress">
+        <div
+          class="progress-bar bg-success"
+          role="progressbar"
+          style={{ width: "75%" }}
+          aria-valuenow="75"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          75%
+        </div>
+      </div>
       <br></br>
-      <h3>Python</h3>
-      <ProgressBar variant="danger" now4={now4} label={`${now4}%`} />
+
+      <h3 style={{ color: "white", textAlign: "start" }}>ReactJS</h3>
+      <div class="progress">
+        <div
+          class="progress-bar bg-success"
+          role="progressbar"
+          style={{ width: "100%" }}
+          aria-valuenow="100"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          100%
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 }
